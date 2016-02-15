bee02d31d9588f8a863c68905c49e8cc1932ee81
Colin Steele <colin.steele20@gmail.com>
2016-02-15 11:27:42 -0500

    added some pygame stuff

--- Tetris.py
+++ Tetris.py
@@ -3,7 +3,24 @@ import pygame
 
 myFactory = TetraFactory.TetraFactory()
 
-#create a cube tetra
+#create a random tetra
 foo = myFactory.newTetra()
 foo.toHash()
 
+pygame.init()
+gameDisplay = pygame.display.set_mode((600,800))
+pygame.display.set_caption('Taylor Swift\'s 1999 Buick LeSabre')
+
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.display.update()
+
+pygame.quit()
+
+
+quit()
