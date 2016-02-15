94a0a15b1d992af595edebf76ecfbafb9023aaad
Colin Steele <colin.steele20@gmail.com>
2016-02-15 12:02:43 -0500

    added some pygame stuff

--- Tetris.py
+++ Tetris.py
@@ -1,6 +1,7 @@
 import TetraFactory
 import pygame
 
+
 myFactory = TetraFactory.TetraFactory()
 
 #create a random tetra
@@ -10,17 +11,24 @@ foo.toHash()
 pygame.init()
 gameDisplay = pygame.display.set_mode((600,800))
 pygame.display.set_caption('Taylor Swift\'s 1999 Buick LeSabre')
+bg = pygame.image.load('1993-buick-lesabre-7.jpg')
 
+white = (255,255,255)
+black = (10,10,10)
 
 gameExit = False
 
+#gameDisplay.fill(black)
+
 while not gameExit:
     for event in pygame.event.get():
-        print(event)
+        gameDisplay.blit(bg, (0, 0))
+        pygame.display.update()
 
-pygame.display.update()
+        if event.type == pygame.QUIT:
+            gameExit = True
+
 
 pygame.quit()
-
 
 quit()
